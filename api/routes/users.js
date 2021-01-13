2308f7fc99aea27b04477842fccc0e90950e4dfc
michalKoczewski <m.k0czewsk11@gmail.com>
2021-01-13 10:22:29 +0100

    Update users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
-const {check, validationResult, body} = require("express-validator");
+const {check, validationResult} = require("express-validator");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 
