e746cf8f2a15f3fda5c0da312549801ec2d16517
michalKoczewski <m.k0czewsk11@gmail.com>
2021-01-29 12:07:02 +0100

    newpost

--- api/models/post.js
+++ api/models/post.js
@@ -8,8 +8,7 @@ const postSchema = mongoose.Schema({
     _id: mongoose.Types.ObjectId,
     postName: {type: String, required: true},
     postDescription: {type: String, required: false},
-    img: {type: String, required: false},
-    comment: {type: commentSchema}
+    img: {type: String, required: false}
 })
 
 module.exports = mongoose.model("Post", postSchema);
